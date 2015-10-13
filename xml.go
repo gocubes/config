601943c401a957042bdd875a1f49293cfcf0d4d5
Frank Kung <kfanjian@gmail.com>
2015-10-13 18:11:28 +0800

    implement "Set" method of xml provider.

--- xml.go
+++ xml.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/xml"
+	"os"
 )
 
 // parse xml string
@@ -19,4 +20,28 @@ func (x *XML) SetRawBytes(raw []byte) {
 
 func (x *XML) Get(data interface{}) error {
 	return xml.Unmarshal(x.raw, data)
+}
+
+func (x *XML) Set(data interface{}) (int, error) {
+	// open file
+	fp, fperr := os.OpenFile(filepath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, os.ModePerm)
+	defer fp.Close()
+	if fperr != nil {
+		return 0, fperr
+	}
+
+	// encode
+	bytes, err := xml.MarshalIndent(&data, "", "    ")
+	if err != nil {
+		return 0, err
+	}
+	bytes = append(bytes, '\n')
+
+	// add xml header
+	data4write := make([]byte, len(xml.Header)+len(bytes))
+	copy(data4write, []byte(xml.Header))
+	copy(data4write[len(xml.Header):], bytes)
+
+	// write
+	return fp.Write(data4write)
 }
