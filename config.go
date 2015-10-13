6356edec44ef89dba3a9a99dec3f6cd3e59f53ed
Frank Kung <kfanjian@gmail.com>
2015-10-13 12:33:20 +0800

    Read file and then, choose provider by format.

--- config.go
+++ config.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"errors"
+	"os"
+)
+
+type Provider interface {
+	SetRawString(raw string)
+	SetRawBytes(raw []byte)
+	Get(data interface{}) error
+}
+
+var (
+	Prefix string
+)
+
+func New(file, format string) (provider Provider, err error) {
+
+	// set config file full path
+	filepath := Prefix + file
+	fp, fperr := os.Open(filepath)
+	if fperr != nil {
+		return nil, fperr
+	}
+
+	// read config file raw data.
+	fstat, _ := fp.Stat()
+	raw := make([]byte, fstat.Size())
+	fp.Read(raw)
+
+	switch format {
+
+	default:
+		return nil, errors.New("Not support this format config file.")
+	}
+	return
+}
