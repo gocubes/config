16bdc93006d029d237ae37fe878046a6844fe469
Frank Kung <kfanjian@gmail.com>
2015-10-14 21:13:49 +0800

    add json provider get test case.

--- config_test.go
+++ config_test.go
@@ -36,6 +36,27 @@ func TestJsonSet(t *testing.T) {
 	}
 }
 
+func TestJsonGet(t *testing.T) {
+	testData := &TestData{}
+
+	// new provider
+	jsonProvider, err := New(jsonFileName, "json")
+	if err != nil {
+		t.Fatalf("[JSON New]%v", err.Error())
+	}
+
+	// get data
+	err = jsonProvider.Get(testData)
+	if err != nil {
+		t.Fatalf("[JSON Get] %v\n", err.Error())
+	}
+
+	// check data
+	if testData.Date != now || testData.Name != name {
+		t.Fatalf("[Check]set Date: %v, Name: %v;but get Date: %v, Name: %v\n", now, name, testData.Date, testData.Name)
+	}
+}
+
 func TestCleanCase(t *testing.T) {
 	t.Logf("Remove json file: %s\n", jsonFileName)
 	os.Remove(jsonFileName)
