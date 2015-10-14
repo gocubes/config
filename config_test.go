22d43752f9eb0c574c818332c1496008045035c4
Frank Kung <kfanjian@gmail.com>
2015-10-14 18:52:14 +0800

    add test case for json provider set method.

---
+++ config_test.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	jsonFileName string = time.Now().Format("200601020304.json")
+	jsonProvider Provider
+)
+
+type TestData struct {
+	Date time.Time
+	Name string
+}
+
+func TestJsonSet(t *testing.T) {
+	testData := &TestData{
+		Date: time.Now(),
+		Name: "default",
+	}
+
+	// new provider
+	jsonProvider, err := New(jsonFileName, "json")
+	if err != nil {
+		// t.Logf("[JSON New]%v", err.Error())
+		t.Fatalf("[JSON New]%v", err.Error())
+		return
+	}
+
+	n, err := jsonProvider.Set(*testData)
+	if err != nil {
+		t.Fatalf("[JSON]write %v bytes, error: %v\n", n, err)
+	}
+}
